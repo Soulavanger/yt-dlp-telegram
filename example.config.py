0268c4d7268687d50b7a55847f1c9b7fc00e4898
Soulavanger <121586876+Soulavanger@users.noreply.github.com>
2023-06-20 23:05:00 +0530

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,3 +1,3 @@
-token = ""  # telegram token
-logs = 2390049  # logs channel id, if none set to None
-max_filesize = 50000000  # bytes
+token = "6275216401:AAHE6NmuaI7QSr9ztEhP4hEiHB-jy_CObAM"  # telegram token
+logs =  -1001821807064  # logs channel id, if none set to None
+max_filesize = 1610612736   # bytes
